8e612da7d48d4fdc8fa3c84eb4eb883e38ff5a15
muqadas9440 <143442776+muqadas9440@users.noreply.github.com>
2025-02-25 14:56:45 +0500

    initaill commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import HeroSection from './Components/HeroSection'
+import About from './Components/About'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <HeroSection/>
+      <About/>
+    </div>
+  )
+}
+
+export default App
